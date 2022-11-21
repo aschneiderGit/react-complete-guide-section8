b70a05a4157bce40e5faa1f27537e3d00c934724
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-11-21 10:39:53 +0100

    Components UI

---
+++ src/component/UI/Button.js
@@ -0,0 +1,11 @@
+import style from './Button.module.css';
+
+function Button(props) {
+	return (
+		<button className={style.button} type={props.type} onClick={props.onClick}>
+			{props.children}
+		</button>
+	);
+}
+
+export default Button;
